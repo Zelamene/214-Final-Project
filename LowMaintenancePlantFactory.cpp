794ea3a45885c1ac94a65944b414f7e329ebc0a0
Tafadzwa Musiiwa <tafadzwamusiiwa08@gmail.com>
2025-10-30 12:16:13 +0000

    Update createFloweringPlant and createTree to use parameters

--- LowMaintenancePlantFactory.cpp
+++ LowMaintenancePlantFactory.cpp
@@ -5,30 +5,16 @@
 #include "CherryBlossom.h"
 #include <iostream>
 
-FloweringPlant* LowMaintenancePlantFactory::createFloweringPlant(){
-    std::cout << "Creating a low maintenance flowering plant (Rose)" << std::endl;
-
-    std::string color;
-    std::cout << "Enter color for your Rose (e.g., red, white, yellow): ";
-    std::cin >> color;
-
-    double price;
-    std::cout << "Enter price of a Flowering Plant : ";
-    std::cin >> price;
+FloweringPlant *LowMaintenancePlantFactory::createFloweringPlant(double price, string color)
+{
+    cout << "Creating a low maintenance flowering plant (Rose)" << endl;
 
     return new Rose(color, price);
 }
 
-Tree* LowMaintenancePlantFactory::createTree() {
-    std::cout << "Creating a low maintenance tree (Aloe)" << std::endl;
-
-    double height;
-    std::cout << "Enter height of Aloe (in meters): ";
-    std::cin >> height;
-
-    double price;
-    std::cout << "Enter price of a tree : ";
-    std::cin >> price;
+Tree *LowMaintenancePlantFactory::createTree(double height, double price)
+{
+    cout << "Creating a low maintenance tree (Aloe)" << endl;
 
     return new Aloe(height, price);
 }
