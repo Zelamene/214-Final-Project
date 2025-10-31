3cbfef226651aa6b2279ca879a8f353f4991f19c
Zelamene Shazi <u24742083@tuks.co.za>
2025-10-31 20:59:27 +0200

    Revert "39 bowdecorator and giftwrap should properly handle descriptions and …"
    This reverts commit 1cda7e1baa6a4db57e34274f75c4f732b9855470.

--- BowDecorator.cpp
+++ BowDecorator.cpp
@@ -1,21 +1,12 @@
 #include "BowDecorator.h"
-using namespace std;
-BowDecorator::BowDecorator(NurseryPlant *plant) : PlantDecorator(plant) {};
 
-string BowDecorator::getDescription()
-{
-    if (plantToDec)
-    {
-        return plantToDec->getName() + " 🎀";
-    }
-    else
-    {
-        exit(1);
-    }
+BowDecorator::BowDecorator(NurseryPlant* plant) : PlantDecorator(plant){};
+
+std::string BowDecorator::getDescription(){
+    return plantToDec->getDescription() + " in a Bow";
 };
 
-double BowDecorator::calculateCost()
-{
-    double total = plantToDec->getTotal() + 50.0;
+double BowDecorator::calculateCost(){
+    double total = plantToDec->calculateCost() + 50.0;
     return total;
 }
--- GiftWrap.h
+++ GiftWrap.h
@@ -3,31 +3,29 @@
 
 #include "PlantDecorator.h"
 #include <string>
-using namespace std;
 
 /**
  * @class GiftWrap
  * @brief Decorator that adds gift wrapping to a plant
- *
+ * 
  * This class implements the Decorator pattern to add gift wrapping
  * functionality to any NurseryPlant, modifying its description and cost.
  */
-class GiftWrap : public PlantDecorator
-{
+class GiftWrap : public PlantDecorator {
 
 public:
     /**
      * @brief Constructor that wraps a plant with gift wrapping
      * @param plant Pointer to the plant being decorated
      */
-    GiftWrap(NurseryPlant *plant);
-
+    GiftWrap(NurseryPlant* plant);
+    
     /**
      * @brief Gets the description of the plant with gift wrapping
      * @return String describing the gift-wrapped plant
      */
-    string getDescription() override;
-
+    std::string getDescription() override;
+    
     /**
      * @brief Calculates the total cost including gift wrapping
      * @return The total price as a double
