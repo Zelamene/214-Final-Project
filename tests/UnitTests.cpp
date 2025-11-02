06e0b0acb93a4c8393511cdc949868e5235d6203
Tafadzwa Musiiwa <tafadzwamusiiwa08@gmail.com>
2025-11-02 11:06:01 +0200

    Add files via upload

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -6,24 +6,25 @@
 #include "../LowMaintenancePlantFactory.h"
 #include "../NurseryPlant.h"
 #include "../CrateFactory.h"
-TEST_CASE("Factory Pattern - Plant and Container Creation", "[factory]") {
+TEST_CASE("Factory Pattern - Plant and Container Creation", "[factory]")
+{
     HighMaintenancePlantFactory highFactory;
     LowMaintenancePlantFactory lowFactory;
     CrateFactory crateFactory;
 
-    auto *rose = highFactory.createFloweringPlant(45.0, "Red");
+    auto *cherryBlossom = highFactory.createFloweringPlant(45.0);
     auto *aloe = lowFactory.createTree(0.7, 25.0);
     auto *crate = crateFactory.createContainer("Wood");
+    auto *rose = lowFactory.createFloweringPlant(102, "Red");
+    auto *baobab = highFactory.createTree(5, 78);
 
-    REQUIRE(rose != nullptr);
+    REQUIRE(cherryBlossom != nullptr);
     REQUIRE(aloe != nullptr);
     REQUIRE(crate != nullptr);
     REQUIRE(crate->getType() == "Crate");
-    REQUIRE(rose->getType() == "FloweringPlant");
-    REQUIRE(aloe->getType() == "Tree");
+    REQUIRE(cherryBlossom->getColor() == "Pink");
+    REQUIRE(aloe->getName() == "Aloe");
     REQUIRE(rose->getColor() == "Red");
-    
-
+    REQUIRE(baobab->getName() == "Baobab");
+    REQUIRE(baobab->getHeight() == 5);
 }
-    
-    
